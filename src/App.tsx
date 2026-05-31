a37e2f035d2d78ba6b31506d1a7fffd1efb213df
Manunu1 <manuelmandel09@gmail.com>
2026-05-31 16:46:43 -0300

    carpetas creadas definitivamente

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,10 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./Paginas/Login";
 import MisCursos from "./Paginas/Profesores/MisMaterias";
 import MateriaDetalle from "./Paginas/Alumnos/MateriaDetalle";
+import HomeGestor from "./Paginas/Gestor/HomeGestor";
+import MateriasGestor from "./Paginas/Gestor/ProfesoresGestor";
+import AsignacionesGestor from "./Paginas/Gestor/AsignacionesGestor";
+import AlumnosGestor from "./Paginas/Gestor/AlumnosGestor";
 
 function App() {
   return (
@@ -13,6 +17,10 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/profesor" element={<MisCursos />} />
         <Route path="/alumnos" element={<MateriaDetalle />} />
+        <Route path="/gestor" element={<HomeGestor />} />
+        <Route path="/gestor/materias" element={<MateriasGestor />} />
+        <Route path="/gestor/asignaciones" element={<AsignacionesGestor />} />
+        <Route path="/gestor/alumnos" element={<AlumnosGestor />} />
       </Routes>
     </BrowserRouter>
   );
